463bca2080dc80c047d4d990e37c1a4e84952168
Artem Bazyliuk <bazylys.artem@gmail.com>
2022-05-18 14:23:11 +0300

    update

--- app/Mail/NewFormRequest.php
+++ app/Mail/NewFormRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NewFormRequest extends Mailable
+class NewFormRequest extends Mailable implements ShouldQueue
 {
     use Queueable;
     use SerializesModels;
